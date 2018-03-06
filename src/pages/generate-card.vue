0d690e9cab6f234d7f70b4fa11e2681a2ace8848
Normaneil <normaneil.macutay@gmail.com>
2018-03-06 20:00:06 +0800

    Initial commit

---
+++ src/pages/generate-card.vue
@@ -0,0 +1,91 @@
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      someJSONdata: [
+          {
+            id: '639276510421',
+            trxid: '60485621OI10',
+            date: '2010-12-13 10:02:11',
+            card: [ "15","23","32","48","65","14","16","44","58","72","5","22","F","55","62","7","18","33","49","70","8","25","37","57","63"]
+          },
+          {
+            id: '639276510421',
+            trxid: '60485621OI10',
+            date: '2010-12-13 10:02:11',
+            card: [ "15","23","32","48","65","14","16","44","58","72","5","22","F","55","62","7","18","33","49","70","8","25","37","57","63"]
+          },
+          {
+            id: '639276510421',
+            trxid: '60485621OI10',
+            date: '2010-12-13 10:02:11',
+            card: [ "15","23","32","48","65","14","16","44","58","72","5","22","F","55","62","7","18","33","49","70","8","25","37","57","63"]
+          }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.block-bg {
+
+}
+
+.h-cell-bg {
+  color: red;
+  font-weight: bold;
+  background: #ffffff;
+}
+
+.b-cell {
+  border: #ccc solid 1px;
+}
+</style>
+
+
+<template>
+  <f7-page>
+    <f7-navbar title="Generate Card" back-link="Back"></f7-navbar>
+    <f7-block strong>
+
+      <f7-card v-for="(item, key) in someJSONdata" :key="key" >
+        <f7-card-header>{{ item.id }} </f7-card-header>
+        <f7-card-content class="block-bg">
+          <f7-row no-gap>
+            <f7-col width="20" class="text-align-center h-cell-bg b-cell">B</f7-col>
+            <f7-col width="20" class="text-align-center h-cell-bg b-cell">L</f7-col>
+            <f7-col width="20" class="text-align-center h-cell-bg b-cell">O</f7-col>
+            <f7-col width="20" class="text-align-center h-cell-bg b-cell">C</f7-col>
+            <f7-col width="20" class="text-align-center h-cell-bg b-cell">K</f7-col>
+
+            <f7-col width="20" class="text-align-center" v-for="(v, k) in item.card" :key="k" >{{ v }}</f7-col>
+          </f7-row>
+        </f7-card-content>
+        <f7-card-footer>{{ item.trxid }}</f7-card-footer>
+      </f7-card>
+
+    </f7-block>
+
+    <!-- <f7-block v-for="(item, key) in someJSONdata" :key="key">
+      <f7-row no-gap>
+        <f7-col width="20" class="text-align-center h-cell-bg b-cell">B</f7-col>
+        <f7-col width="20" class="text-align-center h-cell-bg b-cell">L</f7-col>
+        <f7-col width="20" class="text-align-center h-cell-bg b-cell">O</f7-col>
+        <f7-col width="20" class="text-align-center h-cell-bg b-cell">C</f7-col>
+        <f7-col width="20" class="text-align-center h-cell-bg b-cell">K</f7-col>
+
+        <f7-col width="20" class="text-align-center" v-for="(v, k) in item.card" :key="k" >{{ v }}</f7-col>
+      </f7-row>
+    </f7-block> -->
+
+
+
+
+    <f7-block strong>
+      <f7-link @click="$f7router.back()">Go back</f7-link>
+    </f7-block>
+  </f7-page>
+</template>
+
---
+++ src/pages/home.vue
@@ -0,0 +1,66 @@
+<template>
+  <f7-page>
+    <f7-navbar>
+      <f7-nav-left>
+        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title>Block Game</f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
+      </f7-nav-right>
+    </f7-navbar>
+
+
+    <f7-block-title>Print Sample</f7-block-title>
+    <f7-block strong>
+      <f7-row>
+        <f7-col>
+          <f7-button fill raised color="green" @click="printMe" >Print</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-block>
+    <f7-list>
+      <f7-list-item link="/generate-card/" title="Generate Card"></f7-list-item>
+    </f7-list>
+  </f7-page>
+</template>
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      someJSONdata: [
+          {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            phone: '111-111-1111'
+          },
+          {
+            name: 'Barry Allen',
+            email: 'barry@flash.com',
+            phone: '222-222-2222'
+          },
+          {
+            name: 'Cool Dude',
+            email: 'cool@dude.com',
+            phone: '333-333-3333'
+          }
+      ]
+    }
+  },
+  methods: {
+    printMe() {
+      console.log('here')
+      // printJS({
+      //     printable: this.someJSONdata,
+      //     properties: ['name', 'email', 'phone'],
+      //     type: 'json',
+      //     gridHeaderStyle: 'color: red;  border: 2px solid #3971A5;',
+      //     gridStyle: 'border: 2px solid #3971A5;'
+      // });
+      printJS('https://6.viki.io/image/3cbf77dfea414e648780c4ed854f7072.jpeg?x=b&s=780x436&q=h&e=t&f=t&cb=1', 'image')
+      //printJS('https://www.google.com.ph/', 'html')
+    }
+  }
+}
+</script>
