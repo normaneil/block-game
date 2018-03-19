1fe42e568cb7587d80b0c8538632258442dad585
Normaneil <normaneil.macutay@gmail.com>
2018-03-19 11:08:25 +0800

    Cordova version initial commit
    Version one of cordova branch file are corrupted

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,31 +1,3 @@
-<template>
-  <f7-page>
-    <f7-navbar>
-      <f7-nav-left>
-        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
-      </f7-nav-left>
-      <f7-nav-title>Block Game</f7-nav-title>
-      <f7-nav-right>
-        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
-      </f7-nav-right>
-    </f7-navbar>
-
-
-    <f7-block-title>Print Sample</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col>
-          <f7-button fill raised color="green" @click="printMe" >Print</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
-    <f7-list>
-      <f7-list-item link="/generate-card/" title="Generate Card"></f7-list-item>
-      <f7-list-item link="/generate-barcode/" title="Generate Barcode"></f7-list-item>
-      <f7-list-item link="/scanner/" title="Scanner"></f7-list-item>
-    </f7-list>
-  </f7-page>
-</template>
 <script>
 export default {
   name: 'home',
@@ -62,7 +34,47 @@ export default {
       // });
       printJS('https://6.viki.io/image/3cbf77dfea414e648780c4ed854f7072.jpeg?x=b&s=780x436&q=h&e=t&f=t&cb=1', 'image')
       //printJS('https://www.google.com.ph/', 'html')
+    },
+
+    printCordova() {
+      alert('Print Cordova')
+      cordova.plugins.printer.check(function (available, count) {
+          alert(available ? 'Found ' + count + ' services' : 'No');
+      });
+
+      var page = '<h1>Hello Document</h1>';
+      cordova.plugins.printer.print(page, 'Document.html');
     }
+
   }
 }
 </script>
+<template>
+  <f7-page>
+    <f7-navbar>
+      <f7-nav-left>
+        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title>Block Game</f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
+      </f7-nav-right>
+    </f7-navbar>
+
+
+    <f7-block-title>Print Sample</f7-block-title>
+    <f7-block strong>
+      <f7-row>
+        <f7-col>
+          <f7-button fill raised color="green" @click="printCordova" >Print</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-block>
+    <f7-list>
+      <f7-list-item link="/generate-card/" title="Generate Card"></f7-list-item>
+      <f7-list-item link="/generate-barcode/" title="Generate Barcode"></f7-list-item>
+      <f7-list-item link="/scanner/" title="Scanner"></f7-list-item>
+    </f7-list>
+  </f7-page>
+</template>
+
